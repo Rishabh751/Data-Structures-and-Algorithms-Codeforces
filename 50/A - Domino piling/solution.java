f5717f38cef00884adfb37a5c14bd9300d274091
Rishabh Raj <176932192+Rishabh751@users.noreply.github.com>
2026-08-28 22:23:07 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Prob_50A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int x = (m * n) / 2;
+        System.out.println(x);
+        sc.close();
+    }
+}
